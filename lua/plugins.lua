e69d77d3ef33f2de18a23f783e5db8a8d9050d7a
Matt Smith <matthew.j.smith2@outlook.com>
2023-07-08 17:36:59 -0700

    Adding neovim files

---
+++ lua/plugins.lua
@@ -0,0 +1,34 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    -- You add plugins here
+    -- Lazy Loading:
+    -- Load on specific commands
+    use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
+
+    -- Load on an autocommand event
+    use {'andymass/vim-matchup', event = 'VimEnter'}
+
+    -- Vim Airline
+    use 'vim-airline/vim-airline'
+
+    -- NerdTree
+    use 'preservim/nerdtree'
+
+    -- Auto Complete Commands
+    use {
+        'gelguy/wilder.nvim',
+        config = function()
+        end,
+    }
+
+    use 'preservim/tagbar'
+
+    use 'ryanoasis/vim-devicons'
+
+    -- docs: https://github.com/neoclide/coc.nvim
+    use {'neoclide/coc.nvim', branch = 'master', run = 'yarn install --frozen-lockfile'}
+end)
