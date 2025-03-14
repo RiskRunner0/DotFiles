f86350255c8384bb08e7cea2125b28762f286eb8
Matt Smith <matthew.j.smith2@outlook.com>
2025-03-13 18:41:42 -0700

    Adding vim bindings to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    
-    # Add go bin to PATH
-    set -x PATH ~/go/bin $PATH
+  # Commands to run in interactive sessions can go here
+
+  # Add go bin to PATH
+  set -x PATH ~/go/bin $PATH
+  set -g fish_key_bindings fish_vi_key_bindings
 end
